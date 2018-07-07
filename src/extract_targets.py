3ae0e74c1f770e7086e8d333ab10ba9a257af4b0
okz12 <osmanzubair658@gmail.com>
2018-07-07 14:34:02 +0100

    sobol seq gen

--- src/extract_targets.py
+++ src/extract_targets.py
@@ -10,20 +10,37 @@ from utils_misc import  model_load_dir
 from mnist_loader import search_train_data, search_retrain_data, search_validation_data, train_data, test_data, batch_size
 
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--model_file', dest = "model", help = "Path to model to extract from", required = True)
-args = parser.parse_args()
-model_file = args.model
 
 
-train_data_full = Variable(train_data(fetch='data')).cuda()
+def save_targets(model_file):
+    train_data_full = Variable(train_data(fetch='data')).cuda()
 
-loaded_model = torch.load(model_load_dir + model_file)
-layer_targets = loaded_model.kd_layer_targets(train_data_full)
+    loaded_model = torch.load(model_load_dir + model_file + ".m")
+    layer_targets = loaded_model.kd_layer_targets(train_data_full)
 
-model_name = model_file.split(".")[0]
-if not os.path.exists("{}{}_targets".format(model_load_dir, model_name)):
-    os.makedirs("{}{}_targets".format(model_load_dir, model_name))
-    
-for layer in layer_targets:
-    torch.save(layer_targets[layer], "{}{}_targets/{}.m".format(model_load_dir, model_name, layer))
+    if not os.path.exists("{}{}_targets".format(model_load_dir, model_file)):
+        os.makedirs("{}{}_targets".format(model_load_dir, model_file))
+        
+    for layer in layer_targets:
+        torch.save(layer_targets[layer], "{}{}_targets/{}.m".format(model_load_dir, model_file, layer))
+
+def get_targets(model_file, temp):
+    if not os.path.exists("{}{}_targets".format(model_load_dir, model_file)):
+        save_targets(model_file)
+    loaded_model = torch.load(model_load_dir + model_file + ".m")
+    target_dict = {}
+    layers = list(set([x.replace(".bias","").replace(".weight","") for x in loaded_model.state_dict()]))
+    for layer in layers:
+        output = torch.load("{}{}_targets/{}.out.m".format(model_load_dir, model_file, layer))
+        if (len(output.size())<4):#no temp on conv layers
+            output = (nn.Softmax()(output/temp)).data#dim=1
+        target_dict[layer] = output.clone()
+    return target_dict
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_file', dest = "model", help = "Path to model to extract from", required = True)
+    parser.add_argument('--temp', dest = "temp", help="Temperature: Final softmax temperature for knowledge distillation", required=False, type=(int))
+    args = parser.parse_args()
+    model_file = args.model
+    get_targets(model_file, temp)
---
+++ src/sobol_seq_gen.py
@@ -0,0 +1,35 @@
+'''
+Create 10000 quasi-random low-discrepancy points in a dictionary for 
+1. gamma prior mean (range: 0.001 - 100000, logarithmically spaced)
+2. gamma prior variance (range: 0.01 - 1000, logarithmically spaced)
+3. trade-off parameter for between accuracy and clustering (range: 1e-8 - 1e-3, logarithmically spaced)
+4. knowledge distillation temperature (range: 2 - 20)
+5. number of GMM mixtures (range: 3 - 16)
+Dictionary is saved in the search folder
+'''
+from SALib.sample import saltelli
+import numpy as np
+import pickle
+
+problem = {
+  'num_vars': 5,
+  'names': ['mean', 'var', 'tau', 'temp', 'mixtures'],
+  'bounds': [[np.log(0.001), np.log(100000)],
+    [np.log(0.01), np.log(1000)],
+    [np.log(1e-8), np.log(1e-3)],
+    [1.5,20.49],
+    [2.5,16.49]]
+}
+
+# Generate samples
+param_values = saltelli.sample(problem, 10000)
+
+params = {}
+params['mean'] = np.exp(param_values[:,0])
+params['var'] = np.exp(param_values[:,1])
+params['tau'] = np.exp(param_values[:,2])
+params['temp'] = np.around(param_values[:,3])
+params['mixtures'] = np.around(param_values[:,4])
+
+with open('../search/params.p', 'wb') as handle:
+    pickle.dump("sobol_search.p", handle)
