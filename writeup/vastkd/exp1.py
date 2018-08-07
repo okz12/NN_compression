72dd072a93603c4f45f29f6e1269dba3287576bb
okz12 <osmanzubair658@gmail.com>
2018-08-07 08:52:43 +0000

    exp1 on vast

---
+++ writeup/vastkd/exp1.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.nn.modules import Module
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+import copy
+import pickle
+import model_archs
+
+
+from utils_model import test_accuracy, train_epoch, retrain_sws_epoch, model_prune, get_weight_penalty, layer_accuracy
+from utils_misc import trueAfterN, logsumexp, root_dir, model_load_dir, get_ab, get_sparsity
+from utils_sws import GaussianMixturePrior, special_flatten, KL, compute_responsibilies, merger, sws_prune, sws_prune_l2, sws_prune_copy
+from mnist_loader import search_train_data, search_retrain_data, search_validation_data, train_data, test_data, batch_size
+from extract_targets import get_targets
+from retrain_layer import init_retrain_layer
+from retrain_model import retrain_model
+
+import numpy as np
+import pickle
+
+model, gmp, res = retrain_model(250, 10, 2500, 1250, 1e-6, 5, 16, "LeNet_300_100", "full", 'MSEST', False, "./files")
