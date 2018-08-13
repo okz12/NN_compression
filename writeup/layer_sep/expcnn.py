7abc8924aec8f925c391bf64c589bef09c23aafa
okz12 <osmanzubair658@gmail.com>
2018-08-13 12:02:49 +0100

    fix script

--- writeup/layer_sep/expcnn.py
+++ writeup/layer_sep/expcnn.py
@@ -56,7 +56,7 @@ for tau in tau_list:
 
     targets_dict = get_targets(model_file)
     inputs = train_data(fetch = "data", dset = dset).cuda()
-    targets = torch.cat((targets_dict['conv1.out'].view(-1),targets_dict['conv2.out'].view(-1),targets_dict['fc1.out'].view(-1),targets_dict['fc2.out'].view(-1)), 1).data.cuda()
+    targets = torch.cat((targets_dict['conv1.out'].view(-1),targets_dict['conv2.out'].view(-1),targets_dict['fc1.out'].view(-1),targets_dict['fc2.out'].view(-1)), 0).data.cuda()
     if data_size == "search":
         inputs = inputs[0:10000]
         targets = targets[0:10000]
